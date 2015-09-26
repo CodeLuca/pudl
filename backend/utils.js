7189c3b0015664a1815ac3daefff5ede4cd451b9
Luca Marchal <lucamarchal@gmail.com>
2015-09-26 23:40:14 +0100

    commit

--- backend/utils.js
+++ backend/utils.js
@@ -25,7 +25,7 @@ module.exports = function(app, db) {
       'password': password
     }, function(err, docs) {
       if(!docs[0]) {
-        res.send('User not found');
+        res.send(false);
       } else {
         // Send user data
         utils.findUserData(username, function(response) {
