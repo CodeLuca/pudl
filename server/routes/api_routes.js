1d06e307f7f2f74bbacb91f59deecea0818e366e
Luca Marchal <lucamarchal@gmail.com>
2015-11-06 11:24:59 +0000

    Added user wrapper

--- server/routes/api_routes.js
+++ server/routes/api_routes.js
@@ -12,6 +12,41 @@ module.exports = function(App) {
   debug('exported.');
 
   var api_router = App.Router();
+  
+  /**
+   * @request GET
+   * Get user profile
+   *
+   * @param {string} username
+   */
+   api_router.get('/api/get/user/:username?', function(req, res, next) {
+    // Check if user is logged in.
+    if(!req.session.username) {
+      // Check if user exists
+      App.api.user.isExist(req.params.username)
+        .then(function(doc) {
+          // Send true or false
+          res.send(doc);
+        }, function(err) {
+          // Send error
+          debug(err);
+          res.send(500);
+        });  
+    } else {
+      // Read the users doc.
+      App.api.user.read({
+        'username': req.session.username
+      }).then(function(doc) {
+        // Send doc
+        res.send(doc);
+      }, function(err) {
+        // Send error
+        debug(err);
+        res.send(500);
+      });
+    }
+   });
+
 
   App.Express.use(api_router);
 };
